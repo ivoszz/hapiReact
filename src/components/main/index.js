a770838848f521860f0bb41361030e8cb02eb84b
ivoszz <ivoszz@gmail.com>
2015-08-27 10:02:21 +0200

    Remove HelloMessage component

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 
-import HelloMessage from 'components/hello';
-
-class Main extends React.Component {
+export default class Main extends React.Component {
   render () {
     return (
       <main>
-        <p>Main Content</p>
-        <HelloMessage />
+        <h1>Main Content</h1>
       </main>
     );
   }
 }
-
-export default Main;
